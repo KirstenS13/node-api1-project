aad9c9619c573342aceaea26ceffcb465b6eb6c9
Kirsten S <kirsten@Kirstens-MacBook-Air.local>
2020-08-05 22:23:39 -0400

    finished all the if statements for mvp

--- index.js
+++ index.js
@@ -27,7 +27,11 @@ server.get("/api/users", (req, res) => {
     const users = db.getUsers();
 
     // sends the users back in the response object
-    res.json(users);
+    if (users) {
+        res.json(users);
+    } else {
+        res.status(500).json({ message: "The users information could not be retrieved."});
+    }
 });
 
 // get user by id
@@ -38,21 +42,37 @@ server.get("/api/users/:id", (req, res) => {
     // grab the user by passing getUserById the id
     const user = db.getUserById(id);
 
+    // grab all users
+    const users = db.getUsers();
+
     // send the user back in the response object IF THE ID EXISTS
     if (user) {
+        // send the user back to the client
         res.json(user);
+    } else if (!users.includes(user)) {
+        // let the client know the user does not exist
+        res.status(404).json({ message: "The user with the specified ID does not exist", attemptedId: id });
     } else {
-        res.status(404).json({ message: "That is not a valid id", attemptedId: id });
-    };
+        // let the client know there was a server error
+        res.status(500).json({ message: "The user information could not be retrieved."});
+    }
 });
 
 // create a new user
 server.post("/api/users", (req, res) => {
+    // make sure name and bio are provided
+    if (!req.body.name || !req.body.bio) {
+        res.status(400).json({ message: "Please provide name and bio for the user." });
+    }
     // grab the new user the database created from the object passed in
     const newUser = db.createUser({ name: req.body.name, bio: req.body.bio });
 
     // send back the user created
     res.status(201).json({ message: "New user created", newUser: newUser });
+
+    if (!newUser) {
+        res.status(500).json({ message: "There was an error while saving the user to the database."});
+    }
 });
 
 // delete a user
@@ -63,25 +83,37 @@ server.delete("/api/users/:id", (req, res) => {
     // grab the user by id
     const user = db.getUserById(id);
 
+    // grab all the users
+    const users = db.getUsers();
+
     // confirm the user exists
     if (user) {
         // delete the user
         db.deleteUser(id);
         // let the client know the user was deleted
         res.status(204).end();
-    } else {
+    } else if (!users.includes(user)) {
         // let the client know the user could not be deleted
-        res.status(404).json({ message: "User not found, could not delete", attemptedId: id });
+        res.status(404).json({ message: "The user with the specified ID does not exist", attemptedId: id });
+    } else {
+        // let the client know there was an error with the server
+        res.status(500).json({ message: "The user could not be removed." });
     }
 });
 
 // update a user
 server.put("/api/users/:id", (req, res) => {
+    if (!req.body.name || !req.body.bio) {
+        res.status(400).json({ message: "Please provide name and bio for the user." });
+    }
     // grab the id from the url
     const id = req.params.id;
 
     // grab the user by id from the database to see if they exist
     const user = db.getUserById(id);
+
+    // grab all the users
+    const users = db.getUsers();
 
     // confirm the user exists
     if (user) {
@@ -90,9 +122,11 @@ server.put("/api/users/:id", (req, res) => {
 
         // send back the updated user
         res.status(200).json({ message: "User updated", updatedUser: updatedUser });
-    } else {
+    } else if (!users.includes(user)) {
         // let the client know the user could not be updated
-        res.status(404).json({ message: "User not found, could not update", attemptedId: id });
+        res.status(404).json({ message: "The user with the specified ID does not exist.", attemptedId: id });
+    } else {
+        res.status(500).json({ message: "The user information could not be modified " })
     }
 });
 
